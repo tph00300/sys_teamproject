b0262372eab9851758b315cccaab5332bd8391cb
Seongheon Kim <QDAE@Seongheoms-MacBook-Pro.local>
2019-06-02 00:00:35 +0900

    190601 first commit

--- buzzer_bcm2835_app/buzzer_bcm2835_app.c
+++ buzzer_bcm2835_app/buzzer_bcm2835_app.c
@@ -1,75 +1,26 @@
-#include <wiringPi.h>
-#include <softTone.h>
+#include <bcm2835.h>
 #include <stdio.h>
+#define PIN_PWM RPI_GPIO_P1_12
+#define PWM_CHANNEL 0
+#define RANGE 1024
 
-#define BuzPin    0
+int main (int argc, char **argv) {
+	if(!bcm2835_init())
+		return 1;
 
+	bcm2835_gpio_fsel(PIN_PWM,BCM2835_GPIO_FSEL_ALT5);
+	bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_16);
+	bcm2835_pwm_set_mode(PWM_CHANNEL,1,1);
+	bcm2835_pwm_set_range(0,RANGE);
 
-#define  CL1  131
-#define  CL2  147
-#define  CL3  165
-#define  CL4  175
-#define  CL5  196
-#define  CL6  221
-#define  CL7  248
-
-#define  CM1  262
-#define  CM2  294
-#define  CM3  330
-#define  CM4  350
-#define  CM5  393
-#define  CM6  441
-#define  CM7  495
-
-#define  CH1  525
-#define  CH2  589
-#define  CH3  661
-#define  CH4  700
-#define  CH5  786
-#define  CH6  882
-#define  CH7  990
-
-int song_1[] = {CM3,CM5,CM6,CM3,CM2,CM3,CM5,CM6,CH1,CM6,CM5,CM1,CM3,CM2,
-				CM2,CM3,CM5,CM2,CM3,CM3,CL6,CL6,CL6,CM1,CM2,CM3,CM2,CL7,
-				CL6,CM1,CL5};
-
-int beat_1[] = {1,1,3,1,1,3,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,2,1,1,
-				1,1,1,1,1,1,3};
-
-
-int song_2[] = {CM1,CM1,CM1,CL5,CM3,CM3,CM3,CM1,CM1,CM3,CM5,CM5,CM4,CM3,CM2,
-				CM2,CM3,CM4,CM4,CM3,CM2,CM3,CM1,CM1,CM3,CM2,CL5,CL7,CM2,CM1
-				};
-
-int beat_2[] = {1,1,1,3,1,1,1,3,1,1,1,1,1,1,3,1,1,1,2,1,1,1,3,1,1,1,3,3,2,3};
-
-int main(void)
-{
-	int i, j;
-
-	if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
-		printf("setup wiringPi failed !");
-		return 1; 
+	int direction = 1;
+	int data = 1;
+	while (1) {
+		data = 262;
+		bcm2835_pwm_set_data(PWM_CHANNEL,data);
+		//bcm2835_delay(50);
 	}
 
-	if(softToneCreate(BuzPin) == -1){
-		printf("setup softTone failed !");
-		return 1; 
-	}
-
-	while(1){
-		printf("music is being played...\n");
-
-		for(i=0;i<sizeof(song_1)/4;i++){
-			softToneWrite(BuzPin, song_1[i]);	
-			delay(beat_1[i] * 500);
-		}
-
-		for(i=0;i<sizeof(song_2)/4;i++){
-			softToneWrite(BuzPin, song_2[i]);	
-			delay(beat_2[i] * 500);
-		}	
-	}
-
+	bcm2835_close();
 	return 0;
 }
