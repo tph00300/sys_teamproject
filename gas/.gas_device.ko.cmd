d76f35145ef2d005a256b4ea08849c1255ceca2a
Seongheon_Kim <qdae123@ajou.ac.kr>
2019-06-02 11:56:53 +0900

    gas ~ing

---
+++ gas/.gas_device.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/pi/sys_teamproject/gas/gas_device.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/pi/sys_teamproject/gas/gas_device.ko /home/pi/sys_teamproject/gas/gas_device.o /home/pi/sys_teamproject/gas/gas_device.mod.o ;  true
---
+++ gas/.tmp_versions/gas_device.mod
@@ -0,0 +1,3 @@
+/home/pi/sys_teamproject/gas/gas_device.ko
+/home/pi/sys_teamproject/gas/gas_device.o
+
